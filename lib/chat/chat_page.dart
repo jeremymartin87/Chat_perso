84e0b1988830b8f28c41fcac1feb67adc19064f0
jeremymartin87 <jeremy.martin87620@gmail.com>
2022-12-12 15:28:34 +0100

    Initial commit

---
+++ lib/chat/chat_page.dart
@@ -0,0 +1,67 @@
+import 'package:chat_app_v3/chat/widgets/message_input.dart';
+import 'package:chat_app_v3/chat/widgets/message_tile.dart';
+import 'package:flutter/material.dart';
+import 'models/message.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+  final List<Message> _messages = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Talk to myself',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: _messages.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return MessageTile(
+                            message: _messages[index].text,
+                            user: _messages[index].user);
+                      }),
+                ),
+              ),
+              MessageInput(user: 'A', controller: _controller1, onPressed: (){
+                setState(() {
+                  final message = Message(_controller1.text, 'A');
+                  _messages.add(message);
+                });
+                _controller1.clear();
+              }),
+              const SizedBox(
+                height: 16,
+              ),
+              MessageInput(user: 'B', controller: _controller2, onPressed: (){
+                setState(() {
+                  final message = Message(_controller2.text, 'B');
+                  _messages.add(message);
+                });
+                _controller2.clear();
+              }),
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
